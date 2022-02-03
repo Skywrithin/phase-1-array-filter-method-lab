c8dc4600df83b4fd36acec2424515cc4075b9dd6
Kaleb <kaleb.franken7@gmail.com>
2022-02-02 18:28:17 -0700

    commit tree a9a4d7e55572c7d91533072b51e21de6941a2ffc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(drivers, str){
+    const result = drivers.filter(element => element === str || element === str.toLowerCase());
+    return result
+}
+
+function fuzzyMatch(drivers, str){
+    const result = drivers.filter(element => element.startsWith(str));
+    return result;
+}
+
+function matchName(driver, str){
+    const result = driver.filter(element => element.name === str)
+    return result;
+}
